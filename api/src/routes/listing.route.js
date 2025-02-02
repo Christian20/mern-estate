02438393234e524674542605c25d46253028b213
Christian Steia <christian_steia@yahoo.com>
2025-02-02 13:25:05 +0200

    complete 'update listing' functionality

--- api/src/routes/listing.route.js
+++ api/src/routes/listing.route.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { createListing, deleteListing, updateListing } = require('../controllers/listing.controller');
+const { 
+  createListing, 
+  deleteListing, 
+  updateListing,
+  getListing,
+} = require('../controllers/listing.controller');
 const { verifyToken } = require('../utils/verifyUser');
 
 const listingRouter = express.Router();
@@ -7,6 +12,7 @@ const listingRouter = express.Router();
 listingRouter.post('/create', verifyToken, createListing);
 listingRouter.delete('/delete/:id', verifyToken, deleteListing);
 listingRouter.post('/update/:id', verifyToken, updateListing);
+listingRouter.get('/get/:id', getListing)
 
 module.exports = listingRouter;
 
